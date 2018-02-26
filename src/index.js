c1be4bf1b251d592b1f8ef2e801a6145d2c9a650
soulos410 <solimr111@mail.ru>
2018-02-27 01:36:59 +0300

    smart-calculator/feature: completed

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
 class SmartCalculator {
   constructor(initialValue) {
     this.value = initialValue;
+    this.initialExpression = '' + initialValue;
   }
+
   valueOf(){
     return this.value;
   }
 
   add(number) {
-    this.value += number;
+    this.initialExpression += '+' + number;
+    this.value = eval(this.initialExpression);
     return this;
   }
-  
+
   subtract(number) {
-    this.value -= number;    
+    this.initialExpression += '-' + number;
+    this.value = eval(this.initialExpression);    
     return this;
   }
 
   multiply(number) {
-    this.value *= number;
+    this.initialExpression += '*' + number;
+    this.value = eval(this.initialExpression);
     return this;
   }
 
   devide(number) {
-    this.value /= number;
+    this.initialExpression += '/' + number;
+    this.value = eval(this.initialExpression);
     return this;
   }
 
-  pow(number) {
-    this.value = Math.pow(this.value,number);
+  pow(number) { 
+    this.initialExpression += '**' + number;
+    this.value = eval(this.initialExpression);
     return this;
   }
 }
-
 module.exports = SmartCalculator;
